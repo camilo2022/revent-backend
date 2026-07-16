970520e648eefe1c2f341c9debdfe75b0f03f966
Camilo Acacio <camiloacacio16@gmail.com>
2026-07-15 22:31:09 -0500

    job de sincronizacion de siigo con unico

--- routes/console.php
+++ routes/console.php
@@ -18,30 +18,3 @@ Schedule::command('siigo:export-inventory-scheduled')->dailyAt('07:00');
 Schedule::command('siigo:export-inventory-scheduled')->dailyAt('13:00');
 
 Schedule::command('siigo:sync-unico-scheduled')->hourly()->between('08:00', '22:00');
-
-/*Schedule::call(function () {
-
-    $createdStart = Carbon::now()->subMinutes(20)->format('Y-m-d H:i:s');
-
-    Log::info('Iniciando sincronización Siigo', [
-        'created_start' => $createdStart,
-        'executed_at' => now()->format('Y-m-d H:i:s')
-    ]);
-
-    try {
-        app(SiigoController::class)->execute([
-            'created_start' => $createdStart
-        ]);
-        Log::info('Sincronización Siigo finalizada correctamente', [
-            'created_start' => $createdStart
-        ]);
-    } catch (\Throwable $e) {
-        Log::error('Error en sincronización Siigo', [
-            'message' => $e->getMessage(),
-            'file' => $e->getFile(),
-            'line' => $e->getLine(),
-            'created_start' => $createdStart
-        ]);
-        throw $e;
-    }
-})->everyFifteenMinutes();*/
